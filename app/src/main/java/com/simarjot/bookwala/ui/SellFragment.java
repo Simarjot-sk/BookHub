8b74caaa23585d713dcb8ff13814329a0fbd1e1f
Simarjot-sk <simarjotsinghkalsi@gmail.com>
2020-01-13 16:00:49 +0530

    did major code refactoring in sell fragment

--- app/src/main/java/com/simarjot/bookwala/ui/SellFragment.java
+++ app/src/main/java/com/simarjot/bookwala/ui/SellFragment.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -22,7 +23,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.view.ContextThemeWrapper;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
@@ -37,30 +37,33 @@ import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 
 public class SellFragment extends Fragment {
-    private static final int CAMERA_PERMISSION_CODE = 121;
     private static final int GALLERY_REQUEST_CODE = 420;
     private static final int CAMERA_REQUEST_CODE = 421;
+    private static final int WRITE_PERMISSION_CODE = 11;
+    private static final int CAMERA_PERMISSION_CODE = 12;
     private ImageAdderUtility imageUtil;
     private SharedPreferences.Editor editor;
-
-    //widgets
-    private Button doneButton;
-    private ImageButton addBtn;
+    private Context mContext;
+    private String mImageFilePath;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sell_new, null);
+        mContext = getContext();
         imageUtil = new ImageAdderUtility(getContext(), view);
         SharedPreferences pref = getActivity().getSharedPreferences(BookSharedPrefs.SHARED_PREFS, Context.MODE_PRIVATE);
         editor = pref.edit();
 
-        doneButton = view.findViewById(R.id.done_btn);
-        addBtn = view.findViewById(R.id.add_images_btn);
+        Button doneButton = view.findViewById(R.id.done_btn);
+        ImageButton addBtn = view.findViewById(R.id.add_images_btn);
 
         doneButton.setOnClickListener(v -> {
             ArrayList<String> imageUris = (ArrayList<String>) imageUtil.getImageUris();
@@ -84,27 +87,10 @@ public class SellFragment extends Fragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(), android.R.style.Animation_Dialog));
         final CharSequence[] items = {"Take Photo From Camera", "Choose from Gallery", "Cancel"};
         builder.setItems(items, (dialog, which) -> {
-
             if (items[which].equals("Take Photo From Camera")) {
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
-                } else {
-                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(getContext(),
-                            getContext().getApplicationContext().getPackageName() + ".provider", createImageFile()));
-                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-                }
-
+                dispatchCameraIntent();
             } else if (items[which].equals("Choose from Gallery")) {
-                if (Helper.isReadStoragePermissionGranted(getActivity()) &&
-                        Helper.isReadStoragePermissionGranted(getActivity())) {
-                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                    intent.setType("image/*");
-                    String[] mimeTypes = {"image/jpeg", "image/png"};
-                    intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-                    startActivityForResult(intent, GALLERY_REQUEST_CODE);
-                }
+                dispatchGalleryIntent();
             }
         });
         builder.show();
@@ -124,64 +110,138 @@ public class SellFragment extends Fragment {
                     imageUtil.addImage(croppedImageUri);
                     break;
                 case CAMERA_REQUEST_CODE:
-                    if (currentPhotoPath == null) {
-                        Log.d("nerd", "null hai baba");
-                    }
-                    startCropper(Uri.fromFile(theFile));
+                    startCropper(Uri.fromFile(new File(mImageFilePath)));
                     break;
             }
         } else {
             if (data != null) {
                 Throwable th = UCrop.getError(data);
-                Log.d(EnterPhoneNumberActivity.TAG, "error occured while statiring activity for result " + requestCode, th);
+                Log.d(EnterPhoneNumberActivity.TAG, "error occurred while starting activity for result " + requestCode, th);
             }
             Log.d("nerd", "result code not ok");
         }
     }
 
-    private void startCropper(Uri selectedImage) {
-        File file = Helper.getImageFile();
-        Uri destinationUri = Uri.fromFile(file);
-
-        UCrop.Options options = new UCrop.Options();
-        options.setStatusBarColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
-        options.setToolbarColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
-        options.setFreeStyleCropEnabled(true);
-        options.setToolbarWidgetColor(ContextCompat.getColor(getActivity(), R.color.white));
-        options.setRootViewBackgroundColor(ContextCompat.getColor(getActivity(), R.color.dark_grey));
-        options.setCropFrameColor(ContextCompat.getColor(getActivity(), R.color.dark_grey));
-        options.setActiveWidgetColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
-        options.setRootViewBackgroundColor(ContextCompat.getColor(getActivity(), R.color.black));
-        options.setMaxBitmapSize(1000);
-        options.setCompressionQuality(100);
-
-        UCrop.of(selectedImage, destinationUri)
-                .withMaxResultSize(1000, 1000)
-                .withOptions(options)
-                .start(getActivity().getApplicationContext(), getFragmentManager().findFragmentById(R.id.fragment_container));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Toast.makeText(mContext, "on request permissions called", Toast.LENGTH_SHORT).show();
+        for (int i = 0; i < permissions.length; i++) {
+            String permissionName = permissions[i];
+            int grantCode = grantResults[i];
+            if (permissionName.equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                if (grantCode == PackageManager.PERMISSION_GRANTED) {
+                    dispatchGalleryIntent();
+                } else if (grantCode == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(mContext, "Cannot perform operation without required permissions", Toast.LENGTH_LONG).show();
+                }
+            } else if (permissionName.equals(Manifest.permission.CAMERA)) {
+                if (grantCode == PackageManager.PERMISSION_GRANTED) {
+                    dispatchCameraIntent();
+                } else if (grantCode == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(mContext, "Cannot perform operation without required permissions", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
-    private String currentPhotoPath;
-    private File theFile;
-
-    private File createImageFile() {
-        // Create an image file name
-        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
-        File storageDir = new File(
-                Environment.getExternalStoragePublicDirectory(
-                        Environment.DIRECTORY_DCIM
-                ), "Camera"
-        );
-        File file = null;
-        try {
-            file = File.createTempFile(
-                    imageFileName, ".jpg", storageDir
-            );
-        } catch (IOException ex) {
-            Log.d("nerd", "io exception", ex);
+    private void dispatchGalleryIntent() {
+        if (getWritePermission()) {
+            Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            galleryIntent.setType("image/*");
+            String[] mimeTypes = {"image/jpeg", "image/png"};
+            galleryIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            galleryIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+            startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
         }
-        currentPhotoPath = file.getAbsolutePath();
-        theFile = file;
+    }
+
+    private boolean getWritePermission() {
+        if (Build.VERSION.SDK_INT >= 23) {//permission is automatically granted on sdk<23 upon installation
+            if (mContext.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        WRITE_PERMISSION_CODE);
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    private void dispatchCameraIntent() {
+        if (getCameraPermission()) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(mContext.getPackageManager()) != null) {
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    Log.e(Helper.TAG, "could not create file");
+                }
+
+                String packageName = mContext.getApplicationContext().getPackageName();
+                if (photoFile != null) {
+                    Uri photoUri = FileProvider.getUriForFile(mContext,
+                            packageName + ".provider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+                }
+            }
+        }
+    }
+
+    private boolean getCameraPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (mContext.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = mContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        storageDir.mkdirs();
+        File file = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        mImageFilePath = file.getAbsolutePath();
         return file;
+    }
+
+    private void startCropper(Uri selectedImage) {
+        try {
+            Uri destinationUri = Uri.fromFile(createImageFile());
+            UCrop.Options options = new UCrop.Options();
+            options.setStatusBarColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+            options.setToolbarColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+            options.setFreeStyleCropEnabled(true);
+            options.setToolbarWidgetColor(ContextCompat.getColor(mContext, R.color.white));
+            options.setRootViewBackgroundColor(ContextCompat.getColor(mContext, R.color.dark_grey));
+            options.setCropFrameColor(ContextCompat.getColor(mContext, R.color.dark_grey));
+            options.setActiveWidgetColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+            options.setRootViewBackgroundColor(ContextCompat.getColor(mContext, R.color.black));
+            options.setMaxBitmapSize(1000);
+            options.setCompressionQuality(100);
+
+            UCrop.of(selectedImage, destinationUri)
+                    .withMaxResultSize(1000, 1000)
+                    .withOptions(options)
+                    .start(mContext, getFragmentManager().findFragmentById(R.id.fragment_container));
+        } catch (IOException ex) {
+            Log.e(Helper.TAG, "error while creating destination uri", ex);
+        }
     }
 }
